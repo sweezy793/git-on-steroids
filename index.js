c60061424eb0a0eee2cf67801e50e2dd278565e6
sweezy793 <sarthaks793@gmail.com>
2018-09-06 01:06:49 +0530

    Errors handled, banner added

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 "use strict";	
 
+const file=require('./lib/file');
+const chalk= require('chalk');
+const clear= require('clear');
+const figlet= require('figlet');
 
-const chalk       = require('chalk');
-const clear       = require('clear');
-const figlet      = require('figlet');
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('Git on Steroids', { horizontalLayout: 'full' })
+  )
+);
+
+// if(file.directoryExists('.git')){
+//     console.log(chalk.red('Exists'))
+//     process.exit();
+// }
